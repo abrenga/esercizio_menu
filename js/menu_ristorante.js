ad73aed430594c5e0f3910595bcfd03cb6e017cb
Netta90 <antobrg90@hotmail.it>
2021-04-19 22:54:00 +0200

    inserimento file json e visualizzazione menu.

--- js/menu_ristorante.js
+++ js/menu_ristorante.js
@@ -1,17 +1,77 @@
 /* */
+let contenitoreHtml = document.getElementById("daje");
+let btnS = document.querySelectorAll("btn-m");
+var arrayPiatti = [];
 
 var menu = {
 
-    initMenu : async function () {
+    getMenu: async function () {
         let response = await fetch("file.json");
-        let piatti = response.json();
+        let piatti = await response.json();
 
-        return piatti;
+        arrayPiatti = piatti;
+        this.initMenu();
+        
+    },
+
+    initMenu: function () {
+        arrayPiatti.forEach(piatto => {
+            let article = this.creaPiattoHtml(piatto);
+            contenitoreHtml.innerHTML += article;
+            
+        });
+        this.ascoltaLevento();
+
+    },
+
+
+    creaPiattoHtml: function (item) {
+        return `<article id="article" class="col-6 my-box">
+                    <img src="${item.image}" alt="">
+                    <div class="item-info">
+                        <header class="contenitore">
+                            <h4>${item.title}</h4>
+                            <h5 class="price">${item.prezzo}</h5>
+                        </header>
+                        <p class="text">${item.text}</p>
+                    </div>
+                </article>`;
+    },
+
+    ascoltaLevento: function () {
+        btnS.forEach(btn => {
+            btn.addEventListener("click", function (item) {
+                if (item.tags == "primi") {
+                    this.mostraPrimi()
+                } else if(item.tags == "secondi"){
+                    this.mostraSecondi()
+                } if (item.tags == "panini") {
+                   this.mostraPanini()
+                }
+            })
+        })
+    },
+
+    mostraPrimi: function (item) {
+        let article = document.getElementById("article");
+        if (item.tags !== "primi") {
+            article.classList.add("hide");
+            
+        } else {
+            article.classList.remove("hide")
+        }
     }
+
+
+
+
+
 }
 
 
-this.initMenu().then();
+
+
+
 
 
 
