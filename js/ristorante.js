4e57c831af1cc18597e6c7aea12ca68642573987
Netta90 <58398245+Netta90@users.noreply.github.com>
2021-05-10 16:55:54 +0200

    no

--- js/ristorante.js
+++ js/ristorante.js
@@ -9,34 +9,64 @@ class Menu {
     }
 
 
+    showMenu(container) {
+
+        container.innerHtml = "";
+
+        container.innerHtml =+ Menu.creaPiattoHTML(dish);
+
+    }
+    
+    creaPiattoHTML(dish){
+        return `<article id="article" class="${dish.tag} article col-6 my-box">
+                    <img src="${dish.image}" alt="">
+                    <div class="item-info">
+                        <header class="contenitore">
+                            <h4>${dish.title}</h4>
+                            <h5 class="price">${dish.price}</h5>
+                        </header>
+                        <p class="text">${dish.text}</p>
+                    </div>
+                </article>`;
+
+    }
 
 }
+
+
 
 class Dish {
     constructor(image, title, price, tag) {
         this.image = image,
-        this.title = title,
-        this.price = price,
-        this.tag = tag
+            this.title = title,
+            this.price = price,
+            this.tag = tag
     }
 
-
-    async getMenu() {
-        let response = await fetch("file.json");
-        let dish = await response.json();
-        return dish
-
-    
-        
-    }
 
 }
 
 
-var menu = new Menu();
+
+async function getMenu() {
+    let response = await fetch("file.json");
+    let dishes = await response.json();
+
+    let menu = new Menu;
+
+    dishes.forEach(dish => {
+
+        menu.addDishes(new Dish(dish.image, dish.title, dish.price, dish.tag));
+
+    });
+
+    
+    menu.showMenu(container);
+
+}
 
 
-menu.addDishes(new Dish("", "ciao", "ciao", "ciao"));
+let container = document.getElementById("container")
 
+getMenu().then();
 
-console.log(menu)
